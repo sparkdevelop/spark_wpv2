24ece599161f8b6e813eb242da2cdf66ee5897bb
System Administrator <root@kuhaodeMacBook-Pro.local>
2017-03-08 10:00:35 +0800

    yi ge cu lue de shou ye

---
+++ wp-content/themes/sparkwp/sidebar.php
@@ -0,0 +1,13 @@
+<div id="right-box" style="float: left; width: 400px">
+    <?php if( is_dynamic_sidebar() ){
+        dynamic_sidebar();
+    }else{ ?>
+        <div class="sbox">
+            <h2>分类</h2>
+            <ul>
+                sss
+            </ul>
+        </div>
+
+    <?php } ?>
+</div>
