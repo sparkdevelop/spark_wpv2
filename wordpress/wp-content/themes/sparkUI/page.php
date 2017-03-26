48d8fa8d39a2665a0ba50c984d6454fbfb9546f7
cheriezhang <zhxxuer@163.com>
2017-03-26 16:06:03 +0800

    冲突解决

--- wordpress/wp-content/themes/sparkUI/page.php
+++ wordpress/wp-content/themes/sparkUI/page.php
@@ -6,13 +6,11 @@
  * page.php也是和sidebar一样是默认模板, 包含各个页面内容不同的分类
  *
  */
-<<<<<<< HEAD
+
 global $wpdb;
 $page_qa_id = $wpdb->get_var("SELECT ID FROM $wpdb->posts WHERE post_name = 'qa'");
 $page_project_id = $wpdb->get_var("SELECT ID FROM $wpdb->posts WHERE post_name = 'project'");
 $page_ask_id = $wpdb->get_var("SELECT ID FROM $wpdb->posts WHERE post_name = 'ask'");
-=======
->>>>>>> fc7a8223da6c0c39db187df99f7b6414d608e49a
 ?>
 
 <?php get_header(); ?>
@@ -22,17 +20,12 @@ $page_ask_id = $wpdb->get_var("SELECT ID FROM $wpdb->posts WHERE post_name = 'as
             <!--引入动态模板-->
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
 
-<<<<<<< HEAD
-
-=======
->>>>>>> fc7a8223da6c0c39db187df99f7b6414d608e49a
                 if(is_home() || is_front_page()) { //首页显示“首页侧栏”
                     echo "首页内容";
                 }
                 elseif ( is_page('wiki') ) {//显示wiki侧栏”
                     echo "wiki内容";
                 }
-<<<<<<< HEAD
                 elseif ( is_page($page_qa_id) ) {//显示问答侧栏 参数为pageID 如何自动获取??
                     require "template/qa/QA_content.php";
                 }
@@ -40,15 +33,6 @@ $page_ask_id = $wpdb->get_var("SELECT ID FROM $wpdb->posts WHERE post_name = 'as
                     echo "项目内容";
                 }
                 elseif (is_page($page_ask_id)){
-=======
-                elseif ( is_page(55) ) {//显示问答侧栏 参数为pageID 如何自动获取??
-                    require "template/qa/QA_content.php";
-                }
-                elseif (is_page(62)){
-                    echo "项目内容";
-                }
-                elseif (is_page(96)){
->>>>>>> fc7a8223da6c0c39db187df99f7b6414d608e49a
                     require "template/qa/QA_ask.php";
                 }
                 else{
--- wordpress/wp-content/themes/sparkUI/sidebar.php
+++ wordpress/wp-content/themes/sparkUI/sidebar.php
@@ -2,13 +2,10 @@
 /**
  * sidebar共同的样式
  */
-<<<<<<< HEAD
 global $wpdb;
 $page_qa_id = $wpdb->get_var("SELECT ID FROM $wpdb->posts WHERE post_name = 'qa'");
 $page_project_id = $wpdb->get_var("SELECT ID FROM $wpdb->posts WHERE post_name = 'project'");
 $page_ask_id = $wpdb->get_var("SELECT ID FROM $wpdb->posts WHERE post_name = 'ask'");
-=======
->>>>>>> fc7a8223da6c0c39db187df99f7b6414d608e49a
 ?>
 <!--sidebar.php-->
 <?php
@@ -23,33 +20,20 @@ if ( is_page('wiki') ) {//显示wiki侧栏”
     }
 }
 
-<<<<<<< HEAD
 if ( is_page($page_qa_id) ) {//显示问答侧栏 参数为pageID 如何自动获取??
-=======
-if ( is_page(55) ) {//显示问答侧栏 参数为pageID 如何自动获取??
->>>>>>> fc7a8223da6c0c39db187df99f7b6414d608e49a
     if (function_exists('dynamic_sidebar') && dynamic_sidebar('widget_qasidebar')){
     }
     require "template/qa/QA_sidebar.php";
 }
-<<<<<<< HEAD
 if ( is_page($page_ask_id) ) {//显示问题详情页
-=======
-if ( is_page(96) ) {//显示问题详情页
->>>>>>> fc7a8223da6c0c39db187df99f7b6414d608e49a
     if (function_exists('dynamic_sidebar') && dynamic_sidebar('widget_qa_ask_sidebar')){
     }
     require "template/qa/QA_ask_sidebar.php";
 }
-<<<<<<< HEAD
 if (is_page($page_project_id)){
-=======
-if (is_page(62)){
->>>>>>> fc7a8223da6c0c39db187df99f7b6414d608e49a
     echo "项目边栏";
     if (function_exists('dynamic_sidebar') && dynamic_sidebar('widget_projectsidebar')){
     }
-
 }
 
 ?>
