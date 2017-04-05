b07d8a36e1795d24beb579030217a18cb7a2ced8
sparkdevelop <sparkdevelop@163.com>
2017-04-05 11:14:22 +0800

    解决冲突

--- wp-content/themes/sparkUI/home.php
+++ wp-content/themes/sparkUI/home.php
@@ -11,11 +11,7 @@
 <?php get_header(); ?>
 <div class="container" style="margin-top: 10px">
     <div class="row" style="width: 100%">
-<<<<<<< HEAD
         <div class="col-md-9 col-sm-9 col-xs-9" style="padding-left: 0;padding-right: 0;">
-=======
-        <div class="col-md-8 col-sm-8 col-xs-8">
->>>>>>> 0736b4fcc462d013e0d3eb82e8bbadfb98202f56
             <!--引入动态模板 显示文章标题和提交时间-->
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                 <h2><a href="<?php the_permalink(); ?>">
@@ -30,11 +26,7 @@
             <?php endif; ?>
         </div>
 
-<<<<<<< HEAD
         <div class="col-md-3 col-sm-3 col-xs-3 right" id="col3">
-=======
-        <div class="col-md-4 col-sm-4 col-xs-4 right">
->>>>>>> 0736b4fcc462d013e0d3eb82e8bbadfb98202f56
             <?php get_sidebar(); ?>
         </div>
     </div>
