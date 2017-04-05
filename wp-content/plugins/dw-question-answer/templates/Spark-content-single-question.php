d85459bf78dc979d21db7edf5ee0169be2ec4b51
sparkdevelop <sparkdevelop@163.com>
2017-04-05 10:56:04 +0800

    Update Spark-content-single-question.php
    解决冲突

--- wp-content/plugins/dw-question-answer/templates/Spark-content-single-question.php
+++ wp-content/plugins/dw-question-answer/templates/Spark-content-single-question.php
@@ -11,20 +11,12 @@
     <div class="dwqa-question-item" style="padding: 0px 0px;">
     <!--问题标题-->
     <div class="question_title">
-<<<<<<< HEAD
         <h4 class="qa_title ask_topic"><?php echo get_the_title();?></h4>
         <?php if ( dwqa_current_user_can( 'edit_question', get_the_ID() ) ) : ?>
             <?php if ( dwqa_is_enable_status() ) : ?>
                 <span class="dwqa-question-status" style="float:right;">
 <!--					--><?php //_e( '问题状态', 'dwqa' ) ?>
 					<?php _e( '', 'dwqa' ) ?>
-=======
-        <h4><?php echo get_the_title();?></h4>
-        <?php if ( dwqa_current_user_can( 'edit_question', get_the_ID() ) ) : ?>
-            <?php if ( dwqa_is_enable_status() ) : ?>
-                <span class="dwqa-question-status" style="float:right;">
-					<?php _e( '问题状态', 'dwqa' ) ?>
->>>>>>> 0736b4fcc462d013e0d3eb82e8bbadfb98202f56
                     <select id="dwqa-question-status" data-nonce="<?php echo wp_create_nonce( '_dwqa_update_privacy_nonce' ) ?>" data-post="<?php the_ID(); ?>">
 						<optgroup label="<?php _e( 'Status', 'dwqa' ); ?>">
 							<option <?php selected( dwqa_question_status(), 'open' ) ?> value="open"><?php _e( 'Open', 'dwqa' ) ?></option>
@@ -51,11 +43,7 @@
     </div>
     <!--提问人信息-->
     <div>
-<<<<<<< HEAD
         <span style="color: gray">提问人:&nbsp;<a href="<?php get_the_author_link();?>" class="author_link"><?php echo get_the_author();?></a></span>
-=======
-        <span style="color: gray">提问人:&nbsp;<a href="<?php get_the_author_link();?>"><?php echo get_the_author();?></a></span>
->>>>>>> 0736b4fcc462d013e0d3eb82e8bbadfb98202f56
         <span style="color: gray;margin-left: 5px"><?php echo date('n月j日 G:i',get_the_time('U'));?></span>
         <span class="scan_count" style="margin-left: 0px;float:right; ">浏览<?php echo dwqa_question_views_count();?></span>
         <span class="ask_count" style="margin-right: 20px;float: right;">回答<?php echo dwqa_question_answers_count();?></span>
