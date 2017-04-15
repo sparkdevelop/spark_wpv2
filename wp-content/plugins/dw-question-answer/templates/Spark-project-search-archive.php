0f2b4c98e7747ddb0780227f22e14e6cf3aae03a
sparkdevelop <sparkdevelop@163.com>
2017-04-15 18:45:24 +0800

    更新问答搜索页面的样式

--- wp-content/plugins/dw-question-answer/templates/Spark-project-search-archive.php
+++ wp-content/plugins/dw-question-answer/templates/Spark-project-search-archive.php
@@ -2,24 +2,20 @@
 $search_word=$_GET['s'];
 ?>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-2 col-sm-2 col-xs-2">
+   <div style="margin-top: 20px;"class="clearfix">
+        <div style="float: left;">
             <?php
             if ( has_post_thumbnail() ) { ?>
                 <?php the_post_thumbnail(array(220,150)); ?> <?php } else {?>
-                <img src="<?php bloginfo('template_url'); ?>/img/thumbnail.png" alt="封面" height="150"/>
+                <img src="<?php bloginfo('template_url'); ?>/img/thumbnail.png" alt="封面" height="100" width="180"/>
             <?php } ?>
         </div>
-        <div class="col-md-7 col-sm-7 col-xs-7">
-            <div style="height: 1px;background-color: lightgray"></div>
-
-            <div style="height: 50px;font-size: 20px;color: black;">
-                <b><a href="<?php the_permalink(); ?>" style="color: black"><?php echo str_replace($search_word,'<font color=red>'.$search_word.'</font>',get_the_title()); ?></a></b>
+        <div style="float: left;margin-left:20px;width: 76%;">
+            <div class="project-title">
+                <a class="project-title" href="<?php the_permalink(); ?>" style="color: black"><?php echo str_replace($search_word,'<font color=red>'.$search_word.'</font>',get_the_title()); ?></a>
             </div>
-
-            <p><?php echo str_replace($search_word,'<font color=red>'.$search_word.'</font>',get_the_excerpt()); ?><a href="<?php the_permalink(); ?>" class="button right">阅读全文</a></p>
+            <p style="word-wrap: break-word"><?php echo str_replace($search_word,'<font color=red>'.$search_word.'</font>',get_the_excerpt()); ?><a href="<?php the_permalink(); ?>" class="button right" style="color: #fe642d;">阅读全文</a></p>
         </div>
+        <div style="height: 1px;background-color: #dcdcdc;margin-top: 120px"></div>
     </div>
-</div>
 
