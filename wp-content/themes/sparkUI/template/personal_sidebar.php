4d29a08fa5f16f107e90bbe893c405175788984a
yanshuai <1980740251@qq.com>
2017-05-04 09:48:33 +0800

    修改头像按钮

--- wp-content/themes/sparkUI/template/personal_sidebar.php
+++ wp-content/themes/sparkUI/template/personal_sidebar.php
@@ -44,7 +44,7 @@ $admin_url=admin_url('admin-ajax.php');
         </div>
         <div id="changeAvaDiv" style="display: none;margin-top: 20px;">
             <form class="form-horizontal" role="form" name="updateAva" enctype="multipart/form-data" method="post" action="<?php echo esc_url(self_admin_url('profile-process-avatar.php')); ?>">
-                <a href="javascript:;" class="a-upload">修改头像<input type='file' name='simple-local-avatar' id='simple-local-avatar'/></a>
+                <a href="javascript:;" class="a-upload">选择头像<input type='file' name='simple-local-avatar' id='simple-local-avatar'/></a>
                 <a href="javascript:;" class="a-upload">保存<input type='submit' class='btn btn-default' name='submit' value='保存'/></a>
                 <input type="text" placeholder="文件名" id="aim" class="upload-filename"/>
             </form>
