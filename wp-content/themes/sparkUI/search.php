28191ce1d8130e3385d922e8df1ef8dac5fe5fe5
cheriezhang <zhxxuer@163.com>
2017-05-27 09:14:03 +0800

    control login

--- wp-content/themes/sparkUI/search.php
+++ wp-content/themes/sparkUI/search.php
@@ -1,4 +1,8 @@
-<?php get_header(); ?>
+<?php
+    if (!is_user_logged_in()) {
+    wp_redirect( home_url().'/wp-login.php' );
+    }
+    get_header(); ?>
 <div class="container" style="margin-top: 10px">
     <div class="row" style="width: 100%">
         <div class="col-md-9 col-sm-9 col-xs-12" id="col9">
