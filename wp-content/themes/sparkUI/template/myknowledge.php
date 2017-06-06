fe9a70cf991bc77a07f53dc2ac31d702931cd38f
cheriezhang <zhxxuer@163.com>
2017-06-06 09:53:31 +0800

    fix bugs

--- wp-content/themes/sparkUI/template/myknowledge.php
+++ wp-content/themes/sparkUI/template/myknowledge.php
@@ -1,6 +1,6 @@
 <?php
 $user_id = get_current_user_id();
-$jsonString = jsonGenerate(1);
+$jsonString = jsonGenerate($user_id);
 //$jsonString = sideJSONGenerte(1,"");
 ?>
 <div id="chart" style="width:800px;height: 700px"></div>
