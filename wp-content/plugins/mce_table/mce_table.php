e9c914b8f7dbe68e4d2214711c989f78d78b21d8
ZYLBless <zylbless@163.com>
2017-07-06 17:57:34 +0800

    插入代码和生成wiki

--- wp-content/plugins/mce_table/mce_table.php
+++ wp-content/plugins/mce_table/mce_table.php
@@ -1,10 +1,10 @@
 <?php
 /*
-Plugin Name: TinyMCE Table
+Plugin Name: TinyMCE Plugins
 Plugin URI: http://www.sparkspace.net
-Description: Add Table to TinyMCE.
+Description: Add Plugins to TinyMCE.
 Author: zyl
-Version: 1.0
+Version: 2.0
 Author URI: http://www.sparkspace.net
 */
 
@@ -22,3 +22,30 @@ function mce_table($buttons) {
     return $buttons;
 }
 
+add_filter('mce_external_plugins', 'mce_code_plugin' );
+
+function mce_code_plugin(  $plugins ) {
+    $plugins['code'] = plugin_dir_url( __FILE__ ) .'code/plugin.js';
+
+    return $plugins;
+}
+
+add_filter("mce_buttons", "mce_code");
+function mce_code($buttons) {
+    $buttons[] = 'code';
+    return $buttons;
+}
+
+add_filter('mce_external_plugins', 'mce_codesample_plugin' );
+
+function mce_codesample_plugin(  $plugins ) {
+    $plugins['codesample'] = plugin_dir_url( __FILE__ ) .'codesample/plugin.js';
+
+    return $plugins;
+}
+
+add_filter("mce_buttons", "mce_codesample");
+function mce_codesample($buttons) {
+    $buttons[] = 'codesample';
+    return $buttons;
+}
