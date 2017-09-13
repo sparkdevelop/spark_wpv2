95c7d6eb678f0582b2bb0afd4ba497a05b95669b
ZYLBless <zylbless@163.com>
2017-09-13 17:33:46 +0800

    fix login CSS

--- wp-content/plugins/wechat-social-login/templates/account/login-bar.php
+++ wp-content/plugins/wechat-social-login/templates/account/login-bar.php
@@ -7,10 +7,13 @@ $redirect=$data['redirect'];
 $channels =XH_Social::instance()->channel->get_social_channels(array('login'));    
 ?>
 <div class="xh_social_box" style="clear:both;">
+    <p>第三方登录</p>
+    <div style="text-align: center;">
    <?php 
     foreach ($channels as $channel){
         ?>
         <a href="<?php echo XH_Social::instance()->channel->get_authorization_redirect_uri($channel->id,$redirect);?>" rel="noflow" style="background:url(<?php echo $channel->icon?>) no-repeat transparent;" class="xh_social_login_bar" title="<?php echo $channel->title;?>"></a>
         <?php 
     }?>
-</div><?php 
+    </div>
+</div><?php
