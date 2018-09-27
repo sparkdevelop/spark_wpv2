b4d81c559f04b96b5fd5808d111431cb2439de83
zhy <1368737476@qq.com>
2018-09-27 16:10:09 +0800

    chain_log3

--- wp-content/themes/sparkUI/url_daima.php
+++ wp-content/themes/sparkUI/url_daima.php
@@ -6,40 +6,17 @@
  * Time: 11:26
  */
 
-//require_once( "D:/wamp/www/wordpress/wp-blog-header.php");
-//global $wpdb;
+require_once( "D:/wamp/www/wordpress/wp-blog-header.php");
+require_once( "D:/wamp/www/wordpress/wp-load.php");
+require_once( "D:/wamp/www/wordpress/wp-config.php");
+global $wpdb;
 
 $url = $_GET['url'];
 $page= $_GET['page'];
 $time = date('Y-m-d H:i:s');
-//$file = "chain_log.txt";
 $id= get_current_user_id();
-//$name =$wpdb->get_var( "SELECT user_login FROM `$wpdb->users` WHERE `user` = '$id'");
-//$handle =fopen($file,'a');
-//fwrite($handle,"\r\n");
-//fwrite($handle,"URL:");
-//fwrite($handle,"$url" . ",");
-//fwrite($handle,"time:");
-//fwrite($handle,"$time" . ",");
-//fwrite($handle,"id:");
-//fwrite($handle,"$id" . ",");
 
-$con = mysqli_connect("localhost","root","qingfeng");
-$con->query("SET NAMES utf8");
-if (!$con)
-{
-    fwrite($handle,"die");
-}
-$db_selected =mysqli_select_db($con,"ss");
+$do=$wpdb->insert( 'chain_log', array('url'=>$url, 'time' => $time,'user_id' => $id,'page' => $page ) );
 
-$sql="INSERT INTO `chain_log`( `url`,`time`,`uesr_id`,`page`) VALUES ('$url','$time','$id','$page')";
 
-mysqli_query($con,$sql);
-
-mysqli_close($con);
-
-//fclose($handle);
-
-//$wpdb->insert( $wpdb->prefix .'chain_log', array('url'=>$url, 'time' => $time, 'user_id' => $id));
-
-header("location: $url");  // http://localhost/wordpress/?page_id=15106
+header("location: $url");
