7f7e9e2367de788d5fb6b44353ef0cd7ecf5e0de
ZYLBless <zylbless@163.com>
2019-06-16 17:34:15 +0800

    添加移动端多校入口

--- wp-content/themes/sparkUI/functions.php
+++ wp-content/themes/sparkUI/functions.php
@@ -7074,6 +7074,45 @@ function wp_info_check($login) {
     }
 }
 
+/* PHP CURL HTTPS POST */
+function curl_post_https(){ // 模拟提交数据函数
+    $data = [
+            'token'=>isset($_POST["token"]) ? $_POST["token"] : '',
+        'source'=>isset($_POST["source"]) ? $_POST["source"] : '',
+        'userid'=>isset($_POST["userid"]) ? $_POST["userid"] : '',
+        'username'=>isset($_POST["username"]) ? $_POST["username"] : '',
+        'usersno'=>isset($_POST["usersno"]) ? $_POST["usersno"] : '',
+        'university'=>isset($_POST["university"]) ? $_POST["university"] : '',
+        'content'=>isset($_POST["content"]) ? $_POST["content"] : '',
+        'activity'=>isset($_POST["activity"]) ? $_POST["activity"] : '',
+        'time'=>isset($_POST["time"]) ? $_POST["time"] : '',
+        'url'=>isset($_POST["url"]) ? $_POST["url"] : '',
+        'otherid'=>isset($_POST["otherid"]) ? $_POST["otherid"] : '',
+        'othername'=>isset($_POST["othername"]) ? $_POST["othername"] : '',
+        'othercontent'=>isset($_POST["othercontent"]) ? $_POST["othercontent"] : '',
+    ];
+    $curl = curl_init(); // 启动一个CURL会话
+    curl_setopt($curl, CURLOPT_URL, 'https://www.attentions.org/index.php/api/opendata/data'); // 要访问的地址
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 1); // 从证书中检查SSL加密算法是否存在
+    curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // 模拟用户使用的浏览器
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // 使用自动跳转
+    curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer
+    curl_setopt($curl, CURLOPT_POST, 1); // 发送一个常规的Post请求
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data); // Post提交的数据包
+    curl_setopt($curl, CURLOPT_TIMEOUT, 30); // 设置超时限制防止死循环
+    curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回
+    $tmpInfo = curl_exec($curl); // 执行操作
+    if (curl_errno($curl)) {
+        echo 'Errno'.curl_error($curl);//捕抓异常
+    }
+    curl_close($curl); // 关闭CURL会话
+    return $tmpInfo; // 返回数据，json格式
+}
+
+add_action('wp_ajax_curl_post_https', 'curl_post_https');
+add_action('wp_ajax_nopriv_curl_post_https', 'curl_post_https');
 
 ////wiki和项目内容处理 去标签化 暂时无用
 //function removeHTMLLabel($post_id){
